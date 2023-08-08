0adb67774c998861b3e2f7a4c03975f35fd01550
Keiran <keiransteward@gmail.com>
2023-08-09 09:56:04 +1000

    organising -> ntierarchitecture

---
+++ spring-boot-example/spring-boot-example/src/main/java/com/keiran/customer/CustomerDataAccessService.java
@@ -0,0 +1,31 @@
+package com.keiran.customer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerDataAccessService implements CustomerDao{
+
+
+    //db
+    private static final List<Customer> customers;
+    static {
+        customers = new ArrayList<>();
+        Customer Alex = new Customer(1, "Alex", "alex@gmail.com", 25);
+        Customer James = new Customer(2, "James", "james@gmail.com", 19);
+        customers.add(Alex);
+        customers.add(James);
+
+    }
+    @Override
+    public List<Customer> selectAllCustomers() {
+        return customers;
+    }
+
+    @Override
+    public Optional<Customer> selectCustomerById(Integer id) {
+        return customers.stream().filter(c -> c.getId().equals(id))
+                .findFirst();
+
+    }
+}
