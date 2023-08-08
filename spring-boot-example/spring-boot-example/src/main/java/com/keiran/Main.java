b2e3d9ae0cdcb03a521a16e1d668f6c74db02321
Keiran <keiransteward@gmail.com>
2023-08-09 09:03:46 +1000

    crud

--- spring-boot-example/spring-boot-example/src/main/java/com/keiran/Main.java
+++ spring-boot-example/spring-boot-example/src/main/java/com/keiran/Main.java
@@ -2,25 +2,115 @@ package com.keiran;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @SpringBootApplication
 @RestController
 public class Main {
-    public static void main(String args[]) {
+
+    //db
+    private static List<Customer> customers;
+    static {
+        customers = new ArrayList<>();
+        Customer Alex = new Customer(1, "Alex", "alex@gmail.com", 25);
+        Customer James = new Customer(2, "James", "james@gmail.com", 19);
+        customers.add(Alex);
+        customers.add(James);
+
+    }
+    public static void main(String[] args) {
+
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public GreetResponse greet() {
-        return new GreetResponse("hello",
-                                  List.of("Java", "Golang", "JavaScript"),
-                                  new Person("Alex", 28, 30_000));
+//    @RequestMapping(path="api/v1/customer", method = RequestMethod.GET)
+    @GetMapping("api/v1/customers")
+    public List<Customer> getCustomers() {
+        return customers;
     }
 
-    record Person (String name, int age, double savings) {}
-    record GreetResponse (String greet, List<String> favProgrammingLanguages, Person person){}
+    @GetMapping("api/v1/customers/{customerId}")
+    public Customer getCustomer(@PathVariable("customerId") Integer customerId) {
+        Customer customer = customers.stream().filter(c -> c.id.equals(customerId))
+                .findFirst()
+                .orElseThrow(
+                        () -> new IllegalArgumentException("customer with id [%s] not found".formatted(customerId)));
+        return customer;
+    }
+    static class Customer {
+        private Integer id;
+        private String name;
+        private String email;
+        private Integer age;
+
+        public Customer(Integer id, String name, String email, Integer age) {
+            this.id = id;
+            this.name = name;
+            this.email = email;
+            this.age = age;
+        }
+
+        public Customer(){}
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public Integer getAge() {
+            return age;
+        }
+
+        public void setAge(Integer age) {
+            this.age = age;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Customer customer = (Customer) o;
+            return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name, email, age);
+        }
+
+        @Override
+        public String toString() {
+            return "Customer{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    ", email='" + email + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+    }
+
 }
+
