e5cf2c7301ec36db0865f3ef352894940dbf1d79
Keiran <keiransteward@gmail.com>
2023-08-09 16:07:11 +1000

    accessing database

--- spring-boot-example/spring-boot-example/src/main/java/com/keiran/Main.java
+++ spring-boot-example/spring-boot-example/src/main/java/com/keiran/Main.java
@@ -1,9 +1,15 @@
 package com.keiran;
 
 
+import com.keiran.customer.Customer;
+import com.keiran.customer.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Main {
@@ -13,8 +19,16 @@ public class Main {
        SpringApplication.run(Main.class, args);
 
     }
+    @Bean
+    CommandLineRunner runner(CustomerRepository customerRepository) {
+        return args -> {
+            Customer Alex = new Customer("Alex", "alex@gmail.com", 25);
+            Customer James = new Customer("James", "james@gmail.com", 19);
 
-
+            List<Customer> customers = List.of(Alex, James);
+            customerRepository.saveAll(customers);
+        };
+    }
 
 
 
